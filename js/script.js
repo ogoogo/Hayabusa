da481a5b1e64a06498a73721a2f48760986e7a2f
ogoogo <ogosae.1018173@docomo.ne.jp>
2022-03-16 15:23:31 +0900

    fix:はやぶさパス変更,タイトル変更途中

--- js/script.js
+++ js/script.js
@@ -10,43 +10,44 @@ gsap.registerPlugin(ScrollTrigger);
 // });
 
 // The start and end positions in terms of the page scroll
-const offsetFromTop1 = innerHeight * 0.5;
-const pathBB1 = document.querySelector("#vec1").getBoundingClientRect();
-const pathBB2 = document.querySelector("#vec2").getBoundingClientRect();
-const pathBB3 = document.querySelector("#vec3").getBoundingClientRect();
-const pathBB4 = document.querySelector("#vec4").getBoundingClientRect();
-const pathBB5 = document.querySelector("#vec5").getBoundingClientRect();
-const pathBB6 = document.querySelector("#vec6").getBoundingClientRect();
-const pathBB7 = document.querySelector("#vec7").getBoundingClientRect();
-const pathBB8 = document.querySelector("#vec8").getBoundingClientRect();
-const pathBB9 = document.querySelector("#vec9").getBoundingClientRect();
+const offsetFromTop1 = innerHeight * 0.25;
+const offsetFromTop2 = innerHeight * 0.5;
+const pathBB1 = document.querySelector("#wholevec1").getBoundingClientRect();
+const pathBB2 = document.querySelector("#wholevec2").getBoundingClientRect();
+// const pathBB3 = document.querySelector("#vec3").getBoundingClientRect();
+// const pathBB4 = document.querySelector("#vec4").getBoundingClientRect();
+// const pathBB5 = document.querySelector("#vec5").getBoundingClientRect();
+// const pathBB6 = document.querySelector("#vec6").getBoundingClientRect();
+// const pathBB7 = document.querySelector("#vec7").getBoundingClientRect();
+// const pathBB8 = document.querySelector("#vec8").getBoundingClientRect();
+// const pathBB9 = document.querySelector("#vec9").getBoundingClientRect();
 const startY1 = pathBB1.top - innerHeight + offsetFromTop1;
-const startY2 = pathBB2.top - innerHeight + offsetFromTop1;
-const startY3 = pathBB3.top - innerHeight + offsetFromTop1;
-const startY4 = pathBB4.top - innerHeight + offsetFromTop1;
-const startY5 = pathBB5.top - innerHeight + offsetFromTop1;
-const startY6 = pathBB6.top - innerHeight + offsetFromTop1;
-const startY7 = pathBB7.top - innerHeight + offsetFromTop1;
-const startY8 = pathBB8.top - innerHeight + offsetFromTop1;
-const startY9 = pathBB9.top - innerHeight + offsetFromTop1;
-const finishDistance1 = pathBB1.height;
+const startY2 = pathBB2.top - innerHeight + offsetFromTop2;
+// const startY3 = pathBB3.top - innerHeight + offsetFromTop1;
+// const startY4 = pathBB4.top - innerHeight + offsetFromTop1;
+// const startY5 = pathBB5.top - innerHeight + offsetFromTop1;
+// const startY6 = pathBB6.top - innerHeight + offsetFromTop1;
+// const startY7 = pathBB7.top - innerHeight + offsetFromTop1;
+// const startY8 = pathBB8.top - innerHeight + offsetFromTop1;
+// const startY9 = pathBB9.top - innerHeight + offsetFromTop1;
+const finishDistance1 = pathBB1.height - offsetFromTop1;
 const finishDistance2 = pathBB2.height;
-const finishDistance3 =pathBB3.height;
-const finishDistance4 = pathBB4.height ;
-const finishDistance5 =  pathBB5.height;
-const finishDistance6 = pathBB6.height;
-const finishDistance7 = pathBB7.height;
-const finishDistance8 = pathBB8.height;
-const finishDistance9 = pathBB9.height;
+// const finishDistance3 =pathBB3.height;
+// const finishDistance4 = pathBB4.height ;
+// const finishDistance5 =  pathBB5.height;
+// const finishDistance6 = pathBB6.height;
+// const finishDistance7 = pathBB7.height;
+// const finishDistance8 = pathBB8.height;
+// const finishDistance9 = pathBB9.height;
 
 // the animation to use
 var tween1 = gsap.to("#haya1", {
   duration: 1, 
   paused: true,
-//   ease: "none",
+  ease: "none",
   motionPath: {
-    path: "#vec1",
-    align: "#vec1",
+    path: "#wholevec1",
+    align: "#wholevec1",
     autoRotate: true,
     alignOrigin: [0.5, 0.5]
   }    
@@ -57,96 +58,96 @@ var tween2 = gsap.to("#haya2", {
     paused: true,
     // ease: "none",
     motionPath: {
-      path: "#vec2",
-      align: "#vec2",
+      path: "#wholevec2",
+      align: "#wholevec2",
       autoRotate: true,
       alignOrigin: [0.5, 0.5]
     }    
   }).pause(0.001);
 
-  var tween3 = gsap.to("#haya3", {
-    duration: 5, 
-    paused: true,
-    // ease: "none",
-    motionPath: {
-      path: "#vec3",
-      align: "#vec3",
-      autoRotate: true,
-      alignOrigin: [0.5, 0.5]
-    }    
-  }).pause(0.001);
+//   var tween3 = gsap.to("#haya3", {
+//     duration: 5, 
+//     paused: true,
+//     // ease: "none",
+//     motionPath: {
+//       path: "#vec3",
+//       align: "#vec3",
+//       autoRotate: true,
+//       alignOrigin: [0.5, 0.5]
+//     }    
+//   }).pause(0.001);
 
-  var tween4 = gsap.to("#haya4", {
-    duration: 5, 
-    paused: true,
-    // ease: "none",
-    motionPath: {
-      path: "#vec4",
-      align: "#vec4",
-      autoRotate: true,
-      alignOrigin: [0.5, 0.5]
-    }    
-  }).pause(0.001);
+//   var tween4 = gsap.to("#haya4", {
+//     duration: 5, 
+//     paused: true,
+//     // ease: "none",
+//     motionPath: {
+//       path: "#vec4",
+//       align: "#vec4",
+//       autoRotate: true,
+//       alignOrigin: [0.5, 0.5]
+//     }    
+//   }).pause(0.001);
 
-  var tween5 = gsap.to("#haya5", {
-    duration: 5, 
-    paused: true,
-    // ease: "none",
-    motionPath: {
-      path: "#vec5",
-      align: "#vec5",
-      autoRotate: true,
-      alignOrigin: [0.5, 0.5]
-    }    
-  }).pause(0.001);
+//   var tween5 = gsap.to("#haya5", {
+//     duration: 5, 
+//     paused: true,
+//     // ease: "none",
+//     motionPath: {
+//       path: "#vec5",
+//       align: "#vec5",
+//       autoRotate: true,
+//       alignOrigin: [0.5, 0.5]
+//     }    
+//   }).pause(0.001);
 
-  var tween6 = gsap.to("#haya6", {
-    duration: 5, 
-    paused: true,
-    // ease: "none",
-    motionPath: {
-      path: "#vec6",
-      align: "#vec6",
-      autoRotate: true,
-      alignOrigin: [0.5, 0.5]
-    }    
-  }).pause(0.001);
+//   var tween6 = gsap.to("#haya6", {
+//     duration: 5, 
+//     paused: true,
+//     // ease: "none",
+//     motionPath: {
+//       path: "#vec6",
+//       align: "#vec6",
+//       autoRotate: true,
+//       alignOrigin: [0.5, 0.5]
+//     }    
+//   }).pause(0.001);
 
-  var tween7 = gsap.to("#haya7", {
-    duration: 5, 
-    paused: true,
-    // ease: "none",
-    motionPath: {
-      path: "#vec7",
-      align: "#vec7",
-      autoRotate: true,
-      alignOrigin: [0.5, 0.5]
-    }    
-  }).pause(0.001);
+//   var tween7 = gsap.to("#haya7", {
+//     duration: 5, 
+//     paused: true,
+//     // ease: "none",
+//     motionPath: {
+//       path: "#vec7",
+//       align: "#vec7",
+//       autoRotate: true,
+//       alignOrigin: [0.5, 0.5]
+//     }    
+//   }).pause(0.001);
 
-  var tween8 = gsap.to("#haya8", {
-    duration: 5, 
-    paused: true,
-    // ease: "none",
-    motionPath: {
-      path: "#vec8",
-      align: "#vec8",
-      autoRotate: true,
-      alignOrigin: [0.5, 0.5]
-    }    
-  }).pause(0.001);
+//   var tween8 = gsap.to("#haya8", {
+//     duration: 5, 
+//     paused: true,
+//     // ease: "none",
+//     motionPath: {
+//       path: "#vec8",
+//       align: "#vec8",
+//       autoRotate: true,
+//       alignOrigin: [0.5, 0.5]
+//     }    
+//   }).pause(0.001);
 
-  var tween9 = gsap.to("#haya9", {
-    duration: 5, 
-    paused: true,
-    // ease: "none",
-    motionPath: {
-      path: "#vec9",
-      align: "#vec9",
-      autoRotate: true,
-      alignOrigin: [0.5, 0.5]
-    }    
-  }).pause(0.001);
+//   var tween9 = gsap.to("#haya9", {
+//     duration: 5, 
+//     paused: true,
+//     // ease: "none",
+//     motionPath: {
+//       path: "#vec9",
+//       align: "#vec9",
+//       autoRotate: true,
+//       alignOrigin: [0.5, 0.5]
+//     }    
+//   }).pause(0.001);
 
 // Listen to the scroll event
 document.addEventListener("scroll", function() {
@@ -162,13 +163,13 @@ function update() {
   // Update our animation
   tween1.progress((scrollY - startY1) / finishDistance1);
   tween2.progress((scrollY - startY2) / finishDistance2);
-  tween3.progress((scrollY - startY3) / finishDistance3);
-  tween4.progress((scrollY - startY4) / finishDistance4);
-  tween5.progress((scrollY - startY5) / finishDistance5);
-  tween6.progress((scrollY - startY6) / finishDistance6);
-  tween7.progress((scrollY - startY7) / finishDistance7);
-  tween8.progress((scrollY - startY8) / finishDistance8);
-  tween9.progress((scrollY - startY9) / finishDistance9);
+//   tween3.progress((scrollY - startY3) / finishDistance3);
+//   tween4.progress((scrollY - startY4) / finishDistance4);
+//   tween5.progress((scrollY - startY5) / finishDistance5);
+//   tween6.progress((scrollY - startY6) / finishDistance6);
+//   tween7.progress((scrollY - startY7) / finishDistance7);
+//   tween8.progress((scrollY - startY8) / finishDistance8);
+//   tween9.progress((scrollY - startY9) / finishDistance9);
   
   // Let the scroll event fire again
   requestId = null;
@@ -208,7 +209,7 @@ gsap.to('.ion1', {
 
   gsap.to(".hayabusaTop",{
       rotation:360,
-      duration:2,
+      duration:7,
       ease:"none",
       repeat:-1,
       scrollTrigger:{
